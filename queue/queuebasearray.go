920c31211c75b0ca7863496c0bf0b2b0cd59c2aa
wanghaoxi3000 <wanghaoxi3000@163.com>
2020-03-23 22:42:06 +0800

    添加用数组实现一个顺序队列

---
+++ queue/queuebasearray.go
@@ -0,0 +1,58 @@
+package queue
+
+import "fmt"
+
+/*
+* 用数组实现一个顺序队列
+ */
+
+// ArrayQueue 基于数组的顺序栈
+type ArrayQueue struct {
+	q        []interface{}
+	capacity int
+	head     int
+	tail     int
+}
+
+// NewArrayQueue 构造一个ArrayQueue
+func NewArrayQueue(n int) *ArrayQueue {
+	return &ArrayQueue{
+		q:        make([]interface{}, n),
+		capacity: n,
+		head:     0,
+		tail:     0,
+	}
+}
+
+// EnQueue 入队
+func (q *ArrayQueue) EnQueue(v interface{}) bool {
+	if q.tail == q.capacity {
+		return false
+	}
+	q.q[q.tail] = v
+	q.tail++
+	return true
+}
+
+// DeQueue 出队
+func (q *ArrayQueue) DeQueue() interface{} {
+	if q.head == q.tail {
+		return nil
+	}
+	v := q.q[q.head]
+	q.head++
+	return v
+}
+
+// String 出队
+func (q *ArrayQueue) String() string {
+	if q.head == q.tail {
+		return "empty queue"
+	}
+	result := "head"
+	for i := q.head; i <= q.tail-1; i++ {
+		result += fmt.Sprintf("<-%+v", q.q[i])
+	}
+	result += "<-tail"
+	return result
+}
