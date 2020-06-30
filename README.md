e562da626aa7813cd20426dec12835c464edb901
wanghaoxi3000 <wanghaoxi3000@163.com>
2020-06-30 15:29:52 +0800

    递归添加输出一组数据的全排列

--- README.md
+++ README.md
@@ -22,3 +22,4 @@
 
 - [实现斐波那契数列](recursion/fibonacci.go)
 - [实现求阶乘n!](recursion/factorial.go)
+- [输出一组数据集合的全排列](recursion/rangeAll.go)
---
+++ recursion/rangeAll.go
@@ -0,0 +1,36 @@
+package recursion
+
+import "fmt"
+
+// 输出一组数据集合的全排列
+type RangeType struct {
+	value []interface{}
+}
+
+func NewRangeArray(n int) *RangeType {
+	return &RangeType{
+		make([]interface{}, n),
+	}
+}
+
+func (slice *RangeType)RangeALL( start int)  {
+	length := len(slice.value)
+	if start == length-1{
+		// 如果已经是最后位置，直接将数组数据合并输出
+		fmt.Println(slice.value)
+	}
+
+	for i:=start; i< length; i++{
+		// i = start 时输出自己
+		// 如果i和start的值相同就没有必要交换
+		if i==start || slice.value[i] != slice.value[start]{
+			//交换当前这个与后面的位置
+			slice.value[i], slice.value[start] = slice.value[start], slice.value[i]
+			//递归处理索引+1
+			slice.RangeALL(start+1)
+			//换回来，因为是递归，如果不换回来会影响后面的操作，并且出现重复
+			slice.value[i], slice.value[start] = slice.value[start], slice.value[i]
+		}
+	}
+}
+
